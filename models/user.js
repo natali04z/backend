3c30d236f7a188934bb43a1a70451ea91bc51f8a
Natali <natalizapata869@gmail.com>
2025-05-26 17:33:58 -0500

    update

--- models/user.js
+++ models/user.js
@@ -13,8 +13,7 @@ const userSchema = new mongoose.Schema({
     },
     password: {
       type: String,
-      minlength: [6, 'Password must be at least 6 characters long'],
-      maxlength: [12, 'Password must be at most 12 characters long']
+      required: true
     },
     role: { 
       type: mongoose.Schema.Types.ObjectId, 
